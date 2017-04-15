46ea5760d93890fc389aedcc292d0792da9ef823
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-15 20:25:37 +0200

    call function on bunny clicked

---
+++ Assets/Scripts/DestroyOnMouseDown.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnMouseDown : MonoBehaviour {
+	void OnMouseDown()
+    {
+        Debug.Log(gameObject);
+    }
+}
