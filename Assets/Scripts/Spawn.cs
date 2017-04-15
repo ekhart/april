21f3fac42e0e0f285a029568970a8ff2c2bbbd11
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-15 20:14:52 +0200

    spawn by to seconds

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public float startTime = 2.0f, 
+				 repeatRate = 2.0f;
+
+	public GameObject go;
+	public Vector3 position;
+
+
+	void Start () {
+		InvokeRepeating("InstantiatePrefab", startTime, repeatRate);
+	}
+	
+	void InstantiatePrefab () {
+		Instantiate(go, position, Quaternion.identity);
+	}
+}
