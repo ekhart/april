db111a6bf86a5f83e6fa9bc7fbb692bd97e1b33a
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-15 20:18:22 +0200

    destroy bunny after 2 seconds

---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour {
+
+	public float timeToDestroy = 2.0f;
+
+	void Start () {
+		Invoke("DestroyMe", timeToDestroy);	
+	}
+	
+	void DestroyMe () {
+		Destroy(gameObject);	
+	}
+}
