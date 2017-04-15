5277bf2b23e83bb0abaaaf6e3266272792d3d36f
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-15 20:05:27 +0200

    bunny random move

---
+++ Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float SpeedMaxX;
+	public float SpeedMaxY;
+
+	private float dx, dy;
+
+	// Use this for initialization
+	void Start () {
+		dx = Random.Range(-SpeedMaxX, SpeedMaxX);
+		dy = Random.Range(-SpeedMaxY, SpeedMaxY);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(dx, dy, 0);
+	}
+}
