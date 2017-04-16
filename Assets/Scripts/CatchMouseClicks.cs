04b6eb17ac63be3988ab262591551e168b1c3149
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-16 13:10:47 +0200

    catch mouse position when down & up

---
+++ Assets/Scripts/CatchMouseClicks.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CatchMouseClicks : MonoBehaviour {
+
+	public Vector3 startPosition, endPosition;
+
+	/// <summary>
+	/// OnMouseDown is called when the user has pressed the mouse button while
+	/// over the GUIElement or Collider.
+	/// </summary>
+	void OnMouseDown()
+	{
+		startPosition = Input.mousePosition;
+	}
+
+	/// <summary>
+	/// OnMouseUp is called when the user has released the mouse button.
+	/// </summary>
+	void OnMouseUp()
+	{
+		endPosition = Input.mousePosition;
+	}
+
+}
