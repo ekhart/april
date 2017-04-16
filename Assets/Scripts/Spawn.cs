f94c13a0fff27830cd746139de6a730da995e895
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-16 12:07:16 +0200

    bunny and chick spawner

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,17 +6,18 @@ public class Spawn : MonoBehaviour {
 	public float repeatRate = 2.0f;
 	
 	public GameObject prefab;
+    private float gatheredTime;
+    private float nextSpawnTime;
 
-	void FixedUpdate () {
-		StartCoroutine(InstantiatePrefab);
-	}
-	
-	private IEnumerator InstantiatePrefab {
-		get {
+    private 
+
+	void Update() {
+		gatheredTime += Time.deltaTime;
+		
+		if (gatheredTime > nextSpawnTime) {
+			gatheredTime = 0;
+			nextSpawnTime = Random.Range(1, 3);
 			Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
-
-			var seconds = Random.Range(1, 3);
-			yield return new WaitForSeconds(seconds);
-		}
+		}		
 	}
 }
