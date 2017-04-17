93d6f540dad004406eb7319f5050cd962f93458a
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-17 11:25:16 +0200

    DestroyOnSlice dont work yet

---
+++ Assets/Scripts/DestroyOnSlice.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DestroyOnSlice : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Line")
+        {
+            // Camera.main.GetComponent<AudioSource>().Play();
+            // Destroy(gameObject);
+            // Instantiate(splashReference, randomPos, transform.rotation);
+ 
+            int value = GetComponent<Points>().Value;
+			GameManager.gm.AddPoints(value);
+			
+			Destroy(gameObject);
+        }
+    }
+}
