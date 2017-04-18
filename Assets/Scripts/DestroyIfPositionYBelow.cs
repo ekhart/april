a2133944d55fee7c6f034b38a19b255c2084caad
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-18 17:27:51 +0200

    destroy if position y below

---
+++ Assets/Scripts/DestroyIfPositionYBelow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyIfPositionYBelow : MonoBehaviour {
+
+	public int y = -20;
+	
+	void Update() {
+        if (gameObject.transform.position.y < y) {
+            Destroy(gameObject);
+        }
+    }
+}
