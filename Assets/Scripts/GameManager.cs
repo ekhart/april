0956213073fa140d8498153e468514aba1a1271b
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-18 17:39:20 +0200

    every second substract second

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,16 @@ using UnityEngine.UI;
 public class GameManager {
 
 	private static GameManager instance = new GameManager();
-
 	public int Points;
+	public int Time = 30;
 	public Text ScoreText;
-	
+	public Text TimeText;
 
 	private GameManager() { 
 		ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
-		
+		TimeText = GameObject.Find("TimeText").GetComponent<Text>();
 	}
-	
+
 	public static GameManager gm {
 		get { return instance; }
 	}
@@ -21,5 +21,10 @@ public class GameManager {
 	public void AddPoints(int points) {
 		Points += points;
 		ScoreText.text = "Score: " + Points;
+	}
+
+	public void SubtractSecond() {
+		Time--;
+		TimeText.text = "Time: " + Time + "s";
 	}
 }
---
+++ Assets/Scripts/RepeatEverySecond.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RepeatEverySecond : MonoBehaviour {
+
+	void Start () {
+		InvokeRepeating("SubtractSecond", 1, 1);	
+	}
+
+	void SubtractSecond() {
+		GameManager.gm.SubtractSecond();
+	}
+	
+}
